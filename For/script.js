4413c27af8b9e85685b5a1616b0564ab3b21a718
Anatoly Knizhentsev <faqexe@icloud.com>
2023-02-17 11:29:43 +0300

    add backwards iteration

--- For/script.js
+++ For/script.js
@@ -62,3 +62,25 @@ for(let i = 0; i < user123.length; i++) {
 
    console.log(user123[i], typeof user123[i]);
 }
+
+
+// backwards iteration & loop in loop
+
+console.log('BACKWARDS ITERATION');
+
+const user123 = ['YouRa', 'Allakhverdov', 1974, 'developer', ['Irina', 'Michael']];
+
+for (let i = user123.length - 1; i >= 0; i--) {
+   console.log(i, user123[i]);
+}
+
+console.log('LOOP IN LOOP');
+
+for (let exercise = 1; exercise <= 3; exercise++) {
+   console.log(`Exercise ${exercise}`);
+
+   for (let rep = 1; rep <= 5; rep++) {
+      console.log(`Ex ${exercise} Rep ${rep}`);
+   }
+   console.log('Out of inner loop');
+}
