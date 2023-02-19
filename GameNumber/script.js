8b583d0688272139201bbf4bd4a497681462f9e8
Anatoly Knizhentsev <faqexe@icloud.com>
2023-02-19 15:22:22 +0300

    start game code

---
+++ GameNumber/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guessingNumber = Number(document.querySelector('.number-input').value);
+
+  if(!guessingNumber) {
+    document.querySelector('.guess-message').textContent = 'Введите число!';
+  }
+});
