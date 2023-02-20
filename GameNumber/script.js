6943d7e50c09274a51c8edbdd39bf94b24643f81
Anatoly Knizhentsev <faqexe@icloud.com>
2023-02-20 11:54:50 +0300

    feat: added logic guessing number

--- GameNumber/script.js
+++ GameNumber/script.js
@@ -1,9 +1,42 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.question').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
 
-  if(!guessingNumber) {
+  if (!guessingNumber) {
     document.querySelector('.guess-message').textContent = 'Введите число!';
+  } else if (guessingNumber === secretNumber) {
+    document.querySelector('.guess-message').textContent = 'Правильно!';
+    score++;
+    document.querySelector('.score').textContent = score;
+  } else if (guessingNumber > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent =
+        'Мое число меньше!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.guess-message').textContent = 'Вы проиграли!';
+    }
+  } else if (guessingNumber < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent =
+        'Мое число больше!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.guess-message').textContent = 'Вы проиграли!';
+    }
   }
 });
+
+document.querySelector('.again').addEventListener('click', function() {
+  location.reload();
+})
