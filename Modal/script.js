86c6ac49127415df2c8f6e6ded12e74da7ce36fc
Anatoly Knizhentsev <faqexe@icloud.com>
2023-02-21 15:49:51 +0300

    feat: release modal window

--- Modal/script.js
+++ Modal/script.js
@@ -4,3 +4,28 @@ const modalWindow = document.querySelector('.modal-window');
 const overlay = document.querySelector('.overlay');
 const btnCloseModalWindow = document.querySelector('.close-modal-window');
 const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
+
+const openModalWindow = function () {
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModalWindow = function () {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+  btnsShowModalWindow[i].addEventListener('click', openModalWindow);
+}
+
+btnCloseModalWindow.addEventListener('click', closeModalWindow);
+
+overlay.addEventListener('click', closeModalWindow);
+
+// Если нажата Esc и окно не содержит класса hidden, то добавляем его
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
+    closeModalWindow();
+  }
+});
