ad9de808c129cf84aecd73081a7249606019af7a
Anatoly Knizhentsev <faqexe@icloud.com>
2023-02-21 14:59:03 +0300

    feat: create modal window and script

---
+++ Modal/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.close-modal-window');
+const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
