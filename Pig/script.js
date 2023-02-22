3d8bb0a2353e9649c3c42bb0d35dcafd8d8b7f0e
Anatoly Knizhentsev <faqexe@icloud.com>
2023-02-22 12:37:43 +0300

    feat: change players

--- Pig/script.js
+++ Pig/script.js
@@ -9,18 +9,20 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0Element = document.querySelector('#current--0');
 const current1Element = document.querySelector('#current--1');
-const player0 = document.querySelector('.player--0');
-const player1 = document.querySelector('.player--1');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 
 //* Начальные настройки игры
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
+const totalScore = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //* Бросаем кубик
-btnRoll.addEventListener('click', function() {
+btnRoll.addEventListener('click', function () {
   //TODO Сгенерировать случайное число
   const diceNumber = Math.trunc(Math.random() * 6) + 1;
   console.log(diceNumber);
@@ -33,9 +35,14 @@ btnRoll.addEventListener('click', function() {
   //TODO Если нет - добавить к счету игрока
   if (diceNumber !== 1) {
     currentScore += diceNumber;
-    current0Element.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    player1.classList.add('player--active');
-    player0.classList.remove('player--active');
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
-})
+});
