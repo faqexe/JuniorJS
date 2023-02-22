ecc3d36246c9a7f24c2422561f45d52a9fe015ee
Anatoly Knizhentsev <faqexe@icloud.com>
2023-02-22 13:29:39 +0300

    feat: added btn Hold

--- Pig/script.js
+++ Pig/script.js
@@ -20,29 +20,61 @@ diceElement.classList.add('hidden');
 const totalScore = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let isPlaying = true;
+
+const switchActivePlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+};
 
 //* Бросаем кубик
 btnRoll.addEventListener('click', function () {
-  //TODO Сгенерировать случайное число
-  const diceNumber = Math.trunc(Math.random() * 6) + 1;
-  console.log(diceNumber);
+  if (isPlaying) {
+    //TODO Сгенерировать случайное число
+    const diceNumber = Math.trunc(Math.random() * 6) + 1;
+    console.log(diceNumber);
 
-  //TODO Отобразить число на кубике
-  diceElement.classList.remove('hidden');
-  diceElement.src = `dice${diceNumber}.png`;
+    //TODO Отобразить число на кубике
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice${diceNumber}.png`;
 
-  //TODO Если число 1, переключить на другого игрока
-  //TODO Если нет - добавить к счету игрока
-  if (diceNumber !== 1) {
-    currentScore += diceNumber;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0Element.classList.toggle('player--active');
-    player1Element.classList.toggle('player--active');
+    //TODO Если число 1, переключить на другого игрока
+    //TODO Если нет - добавить к счету игрока
+    if (diceNumber !== 1) {
+      currentScore += diceNumber;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchActivePlayer();
+    }
+  }
+});
+
+//* Кнопка Оставить
+btnHold.addEventListener('click', function () {
+  if (isPlaying) {
+    //TODO Добавить очки к общему счету текущего игрока
+    totalScore[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      totalScore[activePlayer];
+
+    //TODO Если общий счет >= 100 - Игрок выиграл
+    //TODO Если общий счет < 100 - Переключить на другого игрока
+    if (totalScore[activePlayer] >= 20) {
+      isPlaying = false;
+      diceElement.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchActivePlayer();
+    }
   }
 });
